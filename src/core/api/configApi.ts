797cf50f4f443164f015b55ef40424b820ab64d5
Ilya <pechenka.ivk3@gmail.com>
2025-01-25 20:59:34 +0400

    refactor: removed authorization header

--- src/core/api/configApi.ts
+++ src/core/api/configApi.ts
@@ -2,27 +2,24 @@ import { useAuthStore } from '@/core/stores/useAuthStore.ts'
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
-})
-
-apiClient.interceptors.request.use((config) => {
-  const authStore = useAuthStore()
-  if (authStore.accessToken) {
-    config.headers.Authorization = `Bearer ${authStore.accessToken}`
-  }
-  return config
+  baseURL: 'http://localhost:8080/api/v1'
 })
 
 apiClient.interceptors.response.use(
   (response) => response,
   async (error) => {
     if (error.response?.status === 401) {
+
       const authStore = useAuthStore()
+
+      if (authStore.isRefreshing) {
+        return Promise.reject(error)
+      }
+
       try {
         await authStore.refreshTokenIfNeeded()
-        error.config.headers.Authorization = `Bearer ${authStore.accessToken}`
         return apiClient.request(error.config)
-      } catch {
+      } catch (err) {
         authStore.logoutUser()
         window.location.href = '/sign-in'
       }
