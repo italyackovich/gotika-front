bfe9a69863a1bb32f83c1101d8b19075c90092b4
Ilya <pechenka.ivk3@gmail.com>
2025-01-25 21:08:09 +0400

    feat: added to able change user's avatar and credentials

--- src/core/api/userApi.ts
+++ src/core/api/userApi.ts
@@ -1,5 +1,6 @@
 import type { ChangeAddress } from '@/types/dto/ChangeAddress.ts'
 import apiClient from '@/core/api/configApi.ts'
+import type { ChangeUserCredentials } from '@/types/dto/ChangeUserCredentials.ts'
 
 export const getUser = async (id: number) => {
   const response = await apiClient.get(`/users/${id}`)
@@ -9,4 +10,29 @@ export const getUser = async (id: number) => {
 export const patchUser = async (userId: number | null, changeAddress: ChangeAddress) => {
   const response = await apiClient.patch(`/users/${userId}/patch`, changeAddress)
   return response.data
+}
+
+export const changeUserCred = async (userId: number | null, changeUserCredentials: ChangeUserCredentials) => {
+  console.log(changeUserCredentials)
+  const response = await apiClient.patch(`/users/${userId}/ch-cred`, changeUserCredentials)
+  console.log(response)
+  return response.data
+
+}
+
+export const changeImgUser = async (userId: number | null, file: File) => {
+  const formData = new FormData()
+  formData.append('file', file)
+
+  try {
+    const response = await apiClient.patch(`/users/${userId}/change-img`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      }
+    })
+    return response.data
+
+  } catch (error: any) {
+    console.error(error)
+  }
 }
