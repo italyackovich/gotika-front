6984362a53e0ae1dbfefa861f845e5b0d6df8324
Ilya <pechenka.ivk3@gmail.com>
2025-01-25 21:01:58 +0400

    feat: added loading user data on page reload

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,21 @@
 import MyHeader from '@/core/component/ui/MyHeader.vue'
 import { onMounted } from 'vue'
 import { useRestaurantStore } from '@/core/stores/useRestaurantStore.ts'
+import { useAuthStore } from '@/core/stores/useAuthStore.ts'
+import { getUser } from '@/core/api/userApi.ts'
 
 const restaurantStore = useRestaurantStore()
+const authStore = useAuthStore()
+
+const fetchUser = async () => {
+  if (authStore.userId) {
+    authStore.user = await getUser(Number(authStore.userId))
+  }
+}
 
 onMounted(() => {
   restaurantStore.loadRestaurantList()
+  fetchUser()
 })
 
 </script>
